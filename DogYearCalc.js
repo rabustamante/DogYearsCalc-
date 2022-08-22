507b7763b093db326ecc11d535fa784f47b31a61
Ruben Bustamante <rubenabustamante@gmail.com>
2022-08-21 20:50:19 -0600

    Create DogYearCalc.js

---
+++ DogYearCalc.js
@@ -0,0 +1,18 @@
+// var myAge set to irl age
+let myAge = 22;
+// var named early years set to 2
+let earlyYears =2;
+// earlyyears * 10.5
+earlyYears = earlyYears * 10.5;
+// laters years is set to my age -2
+let laterYears = myAge - 2;
+// later years  mulplty by 4
+laterYears *= 4;
+//debug
+//console.log(earlyYears, laterYears);
+// adding early years and later years and storing it in var
+let myAgeInDogYears = earlyYears +  laterYears;
+
+const myName = 'Ruben'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} old in human years which is ${myAgeInDogYears} years old in dog years`)
